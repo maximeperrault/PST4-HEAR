34c04dd6813c84ff5733c6eba28f926b6cb59176
maximeperrault <maxperrault94@gmail.com>
2015-04-06 21:38:21 +0200

    Second commit
    Server SQL du site et app

---
+++ android_login_api/include/config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Database config variables
+ */
+define("DB_HOST", "projectsysteam19.mysql.db");
+define("DB_USER", "projectsysteam19");
+define("DB_PASSWORD", "Bigdata19");
+define("DB_DATABASE", "projectsysteam19");
+?>
